f7f1d2e99aee8cd18b0f0e9e2d54036f28b628ed
Musharaf Shaik <musharafshaik630@gmail.com>
2026-07-09 08:45:37 +0530

    create auth model

---
+++ src/modules/auth/auth.model.js
@@ -0,0 +1,50 @@
+import { required } from "joi";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 25,
+            required: [true, "Name is required"],
+        },
+        email: {
+            type: String,
+            trim: true,
+            unique: true,
+            required: [true, "email is required"],
+        },
+        password: {
+            type: String,
+            minlength: 8,
+            required: [true, "Password is required"],
+            select: false,
+        },
+        role: {
+            type: String,
+            enum: ["customer", "admin", "seller"],
+            default: "customer",
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        refreshToken: {
+            type: String,
+            select: false,
+        },
+        resetPasswordToken: {
+            type: String,
+            select: false,
+        },
+        resetPasswordExpires: {
+            type: Date,
+            select: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model("User", userSchema);
